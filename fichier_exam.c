d9da7a71bf9ecd2bebc8f231cbcbacfb814d7d83
Sally1700 <fatmi.salwa17@gmail.com>
2025-02-05 14:37:52 +0100

    Definition de mes fonctions

--- fichier_exam.c
+++ fichier_exam.c
@@ -3,7 +3,7 @@
 
 void afficher_menu ;
 
-int recup_valeur; 
+int recup_valeur (int x); 
 
 int main ()
 {
@@ -13,11 +13,18 @@ return 0;
 }
 
 void afficher_menu
-{
+{  
+      printf("Si vous voulez afficher l'adresse IP , veuillez entrer 1.\n");
+      printf("Si vous voulez afficher la table de routage ,veuillez entrer 2.\n");
+      printf("Si vous voulez vérifier la connectivité avec Google , veuillez entrer 3 .\n");
+      printf("Si vous souhaitez quitter ,veuillez entrer 0 .\n");      
+	      
 }
 
-int recup_valeur
-{
 
+int recup_valeur (int x)
+{
+      printf("Veuillez entrer le nombre souhaité:");
+      scanf("%i",&x);
 
 }	
