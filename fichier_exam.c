b720c86b31d918c590e21142c4a08969c93802cd
Sally1700 <fatmi.salwa17@gmail.com>
2025-02-05 14:20:26 +0100

    Corps de mon code

--- fichier_exam.c
+++ fichier_exam.c
@@ -1,1 +1,23 @@
-test
+#include <stdio.h>
+#include <stdlib.h>
+
+void afficher_menu ;
+
+int recup_valeur; 
+
+int main ()
+{
+
+
+return 0;
+}
+
+void afficher_menu
+{
+}
+
+int recup_valeur
+{
+
+
+}	
